63e82bd0796d501670f991f55da7aa9c1c532bf9
csgear <csgear@live.com>
2017-05-28 16:18:42 +0800

    add cracking password

--- python/cracking_password.py
+++ python/cracking_password.py
@@ -1,8 +1,16 @@
 
-from itertools import ...
+from itertools import product
 
 def crackingPassword(digits, k, d):
     def createNumber(digs):
         return "".join(map(str, digs))
 
-    return ...
+    return sorted(list(map(createNumber,
+                    filter(lambda  x: int(createNumber(x) ) % d == 0, product(digits, repeat=k))
+                    )))
+
+if __name__ == '__main__':
+    digits = [1, 5, 2]
+    k = 2
+    d = 3
+    print(crackingPassword(digits, k, d))
